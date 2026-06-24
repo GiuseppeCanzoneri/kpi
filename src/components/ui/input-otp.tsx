a052ef64a65875640db50df4315f1722d7bf19bf
[dyad] <gcanzoneri6@gmail.com>
2026-06-24 16:13:53 +0200

    [dyad] Fixing TypeScript errors and component syntax - wrote 7 file(s)

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -24,33 +24,6 @@ const InputOTPGroup = React.forwardRef<
   React.ElementRef<"div">,
   React.ComponentPropsWithoutRef<"div">
 >(({ className, ...props }, ref) => (
-<dyad-write path="src/components/ui/input-otp.tsx" description="Completing the InputOTP component fix.">
-import * as React from "react"
-import { OTPInput, OTPInputContext } from "input-otp"
-import { Dot } from "lucide-react"
-
-import { cn } from "@/lib/utils"
-
-const InputOTP = React.forwardRef<
-  React.ElementRef<typeof OTPInput>,
-  React.ComponentPropsWithoutRef<typeof OTPInput>
->(({ className, containerClassName, ...props }, ref) => (
-  <OTPInput
-    ref={ref}
-    containerClassName={cn(
-      "flex items-center gap-2 has-[:disabled]:opacity-50",
-      containerClassName
-    )}
-    className={cn("disabled:cursor-not-allowed", className)}
-    {...props}
-  />
-))
-InputOTP.displayName = "InputOTP"
-
-const InputOTPGroup = React.forwardRef<
-  React.ElementRef<"div">,
-  React.ComponentPropsWithoutRef<"div">
->(({ className, ...props }, ref) =>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex items-center", className)}
