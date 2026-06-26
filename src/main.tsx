35a44c9edbe954f71936ebb31c329e7658a10362
[dyad] <gcanzoneri6@gmail.com>
2026-06-26 03:07:34 +0200

    Update 4 files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./globals.css";
+import App from "./App";
 import "./kpi-refresh.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
